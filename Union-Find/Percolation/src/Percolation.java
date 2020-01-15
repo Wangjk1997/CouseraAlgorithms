ba6e903edc2257d3c54022aa1dab4155441e4f1f
wangjk1997 <wangjk_work@163.com>
2020-01-15 17:20:21 -0500

    beginningOfConstructor

--- Union-Find/Percolation/src/Percolation.java
+++ Union-Find/Percolation/src/Percolation.java
@@ -4,8 +4,40 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation 
 {
-	public static void main(String args[]) 
-    { 
-        System.out.println("Hello, World"); 
-    } 
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n)
+    {
+    	theSite = new boolean[n][n];
+    	for(boolean[] line: theSite)
+    	{
+    		for(boolean element: line)
+    		{
+    			element = false;
+    			System.out.println(element);
+    		}
+    	} 	
+    }
+    
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col)
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col)
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col)
+
+    // returns the number of open sites
+    public int numberOfOpenSites()
+
+    // does the system percolate?
+    public boolean percolates()
+
+    // test client (optional)
+    public static void main(String[] args)
+    {
+    	percolation = new Percolation;
+    	
+    }
+    private boolean[][] theSite;
 }
